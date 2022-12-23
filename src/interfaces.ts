3543ec21c61539cba9f746a1f36bc0c688ebca99
Edward Tanguay (windows) <edward@tanguay.info>
2022-12-23 13:34:59 +0100

    switched from jobs to starters

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,55 +1,7 @@
-export interface IJob {
+export interface IStarter {
 	id: number;
-	title: string;
-	company: string;
-	url: string;
-	description: string;
-	skillList: string;
-	skills: ISkill[];
-	todo: string;
-}
-
-export interface ISkill {
 	idCode: string;
-	name: string;
-	url: string;
-	description: string;
-}
-
-export interface ITodo {
-	todoText: string;
-	company: string;
 	title: string;
-	url: string;
-}
-
-export interface ISkillTotal {
-	skill: ISkill;
-	total: number;
-}
-
-export const nullObjectSkill = {
-	idCode: '',
-	name: '',
-	url: '',
-	description: ''
-}
-
-export interface IEditedJob {
-	id: number,
-	title: string;
-	company: string;
-	url: string;
 	description: string;
-	skillList: string;
-	todo: string;
-}
-
-export interface IAddedJob {
-	title: string;
-	company: string;
-	url: string;
-	description: string;
-	skillList: string;
-	todo: string;
+	characteristics: string;
 }
